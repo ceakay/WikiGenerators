350231da379e351beca43275050f84a7d95259de
ceakay <hieyeck@gmail.com>
2020-11-14 17:39:44 -0500

    Update RT_GEAR_PARSER.ps1
    Remove '/' from uiname
    Change uiname '+' to Mk.COUNT
    inserted stuff to provide list of '+'

--- RT_GEAR_PARSER.ps1
+++ RT_GEAR_PARSER.ps1
@@ -10,6 +10,7 @@ $CacheRoot = "$RTroot\\RtlCache\\RtCache"
 #Define component unique
 $ComponentFilter = "*`"ComponentType`"*"
 $GearFile = $RTScriptroot+"\\Outputs\\GearTable.json"
+$WonkyFile = $RTScriptroot+"\\Outputs\\WonkyGear.csv"
 
 ###INIT VARS
 $ComponentObjectList = @()
@@ -18,12 +19,28 @@ $ComponentObjectList = @()
 #construct mega component object list
 $JSONList = Get-ChildItem $CacheRoot -Recurse -Filter "*.json"
 $i = 0
+$WonkyList = @()
 foreach ($JSONFile in $JSONList) {
     Write-Progress -Activity "Collecting Components" -Status "$($i+1) of $($JSONList.Count) JSONs found."
     $JSONRaw = Get-Content $JSONFile.FullName -Raw
     if ($JSONRaw -like $ComponentFilter) {
         try {
-            $ComponentObjectList += $($JSONRaw | ConvertFrom-Json)
+            $JSONObject = $($JSONRaw | ConvertFrom-Json)
+            $JSONObject.Description.UIName = $JSONObject.Description.UIName.Replace("/","")
+            $UINameArray = $($JSONObject.Description.UIName -split (" \+"))
+            if ($UINameArray.Count -gt 1) {
+                $JSONObject.Description.UIName = $UINameArray[0] + " Mk$($UINameArray.Count - 1)"
+                if ($JSONObject.ComponentTags.items -match 'blacklist') {
+                    $BlacklistComponent = $true
+                } else {
+                    $BlacklistComponent = $false
+                }
+                $WonkyList += [pscustomobject]@{
+                    File = $JSONFile.FullName
+                    Blacklist = $BlacklistComponent
+                }
+            }
+            $ComponentObjectList += $JSONObject
         } catch {
             Write-Host $JSONFile
         }
@@ -32,4 +49,5 @@ foreach ($JSONFile in $JSONList) {
 }
 Write-Output "$($ComponentObjectList.Count) components collected"
 #output to file 
-$ComponentObjectList | ConvertTo-Json -Depth 10 > $GearFile
+$ComponentObjectList | ConvertTo-Json -Depth 99 > $GearFile
+$WonkyList | Export-Csv $WonkyFile -NoTypeInformation
