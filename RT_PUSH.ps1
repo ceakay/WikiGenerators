9c96b39a6f3cea0b5675830ca2cae748bb8d0a15
ceakay <hieyeck@gmail.com>
2021-02-18 11:47:08 -0500

    oops

--- RT_PUSH.ps1
+++ RT_PUSH.ps1
@@ -15,15 +15,15 @@ Wait-Process -Id $PIDs #wait for parsers to finish. transcoders may need mulitpl
 $PIDs = @()
 $RefHash = @{}
 
-$MechPID += (Start-Process pwsh -ArgumentList "$RTScriptroot\RT_MECH_WIKI_TRANSCODE.ps1").Id
+$MechPID += (Start-Process pwsh -ArgumentList "$RTScriptroot\RT_MECH_WIKI_TRANSCODE.ps1" -PassThru).Id
 $PIDs += $MechPID
 $RefHash.Add($MechPID,'Mech')
 
-$TankPID = (Start-Process pwsh -ArgumentList "$RTScriptroot\RT_TANK_WIKI_TRANSCODE.ps1").Id
+$TankPID = (Start-Process pwsh -ArgumentList "$RTScriptroot\RT_TANK_WIKI_TRANSCODE.ps1" -PassThru).Id
 $PIDs += $TankPID
 $RefHash.Add($TankPID,'Tank')
 
-$GearPID = (Start-Process pwsh -ArgumentList "$RTScriptroot\RT_GEAR_WIKI.ps1").Id
+$GearPID = (Start-Process pwsh -ArgumentList "$RTScriptroot\RT_GEAR_WIKI.ps1" -PassThru).Id
 $PIDs += $GearPID
 $RefHash.Add($GearPID,'Gear')
 
