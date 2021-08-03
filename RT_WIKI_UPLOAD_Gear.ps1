34dda08ffc1d35a43b7d0381441a6b9b22e1643f
ceakay <hieyeck@gmail.com>
2021-08-03 16:00:24 -0400

    Update RT_WIKI_UPLOAD_Gear.ps1
    unit_affinities automation

--- RT_WIKI_UPLOAD_Gear.ps1
+++ RT_WIKI_UPLOAD_Gear.ps1
@@ -2,6 +2,9 @@
 $PWBRoot = "D:\\PYWikiBot"
 $RTScriptroot = "D:\\RogueTech\\WikiGenerators"
 $titlestartend = "@@@"
+#Unit Affinities Push
+py $PWBRoot\\pwb.py pagefromfile -file:'D:\RogueTech\WikiGenerators\Outputs\Unit_Affinities.utf8' -notitle -force -pt:0 -titlestart:$titlestartend -titleend:$titlestartend
+cls
 #push gear navbox first, then remainder pages
 py $PWBRoot\\pwb.py pagefromfile -file:'D:\RogueTech\WikiGenerators\Outputs\Gear\!Navbox.txt' -notitle -force -pt:0 -titlestart:$titlestartend -titleend:$titlestartend
 cls
